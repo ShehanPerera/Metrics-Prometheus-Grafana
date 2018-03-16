56e0c4e368e2ad7a168617c7fc352e0441f8562f
ShehanPerera <12shehanakalanka@gmail.com>
2018-03-16 12:18:26 +0530

    Changes for SimpleInboundHandler

--- src/main/java/com/github/shehanperera/netty/echo/EchoServerHandler.java
+++ src/main/java/com/github/shehanperera/netty/echo/EchoServerHandler.java
@@ -15,47 +15,66 @@
 package com.github.shehanperera.netty.echo;
 
 import com.codahale.metrics.Timer;
-import io.netty.buffer.ByteBuf;
-import io.netty.buffer.Unpooled;
-import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelInboundHandlerAdapter;
-import io.netty.util.CharsetUtil;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.DefaultFullHttpResponse;
+import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.handler.codec.http.FullHttpResponse;
+import io.netty.handler.codec.http.HttpHeaderNames;
+
+import static io.netty.handler.codec.http.HttpResponseStatus.OK;
+import static io.netty.handler.codec.http.HttpVersion.HTTP_1_1;
+
 /**
  * Handler for Netty-echo-server
  */
 @ChannelHandler.Sharable
-public class EchoServerHandler extends ChannelInboundHandlerAdapter {
+public class EchoServerHandler extends SimpleChannelInboundHandler<FullHttpRequest> {
 
+    private final MetricsServer metricServer = MetricsServer.getInstance();
+
+    private int msgSize;
     private Timer.Context context;
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+
+        ctx.flush();
+    }
     /**
-     *ChannelRead in used to get the request and send the response back
-     * @param msg this is the request msg from client
+     * Message Handler and sending response
+     * @param msg This is the request form client
      */
     @Override
-    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+    protected void channelRead0(ChannelHandlerContext ctx, FullHttpRequest msg)
+            throws Exception {
 
-        MetricsServer metricsServer = MetricsServer.getInstance();
-        context = metricsServer.getResponsesTime().time();
-        metricsServer.getTotallJobs().inc();
-        ByteBuf response = (ByteBuf) msg;
-        metricsServer.getResponseSize().update(response.readableBytes());
-        System.out.println("Server received: " + response.toString(CharsetUtil.UTF_8));
-        ctx.write(response);
-        context.stop();
-    }
+        try {
 
-    @Override
-    public void channelReadComplete(ChannelHandlerContext ctx) {
+            msgSize = msg.content().readableBytes();
+            // Number of all income jobs
+            metricServer.getTotalJobs().inc();
+            // Length of request message calculate by histogram
+            metricServer.getRequestSize().update(msgSize);
+            // Timer start
+            context = metricServer.getResponsesTime().time();
+            // Create response
+            FullHttpResponse response = new DefaultFullHttpResponse(HTTP_1_1, OK, msg.content().copy());
+            response.headers().setInt(HttpHeaderNames.CONTENT_LENGTH, msgSize);
+            //Length of response
+            metricServer.getResponseSize().update(response.content().readableBytes());
 
-        ctx.writeAndFlush(Unpooled.EMPTY_BUFFER).addListener(ChannelFutureListener.CLOSE);
-    }
+            if (ctx != null) {
+                //output
+                ctx.write(response);
+                //Number of success jobs.
+                metricServer.getSuccessJobs().inc();
+            }
 
-    @Override
-    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
-
-        cause.printStackTrace();
-        ctx.close();
+        } finally {
+            //Stop timer
+            context.stop();
+        }
     }
 }
